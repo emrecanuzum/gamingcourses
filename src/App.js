2d274e3c4af2c556addceafe87207278bfa35f2e
Emrecan Üzüm <uzumemrecan@gmail.com>
2023-04-02 01:38:00 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import banner from './assets/banner.png';
+import fifa from './assets/fifa.png';
+import cod from './assets/cod.png';
+import ow from './assets/ow.png';
+import wow from './assets/wow.png';
+import dc from './assets/dc.png';
+import dcDark from './assets/dcb.png';
+import bg from './assets/gradient.png';
+import Aos from "aos";
+import "aos/dist/aos.css";
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    Aos.init({});
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='Row-Header'>
+        <h2>gamingcourses.gg</h2>
+        <img src={dc} alt=""></img>
+        <h1>From Bro's to Pro's</h1>
+      </div>
+
+
+      <div className='Banner' data-aos="fade-in" >
+      <img src={banner} alt=""></img>,
+      </div>
+
+
+      <div className="Landing-Bottom" style={{ backgroundImage:`url(${bg})` }}>
+        <div className="Row"  data-aos="fade-up">
+          <div className="index-6-1">
+            <img src={fifa} alt=""></img>
+          </div>
+          <div className="index-6-1">
+            <img src={cod} alt=""></img>
+          </div>
+          <div className="index-6-1">
+            <img src={ow} alt=""></img>
+          </div>
+          <div className="index-6-1">
+            <img src={wow} alt=""></img>
+          </div>
+        </div>
+      </div>
+
+      <div className='join-discord'>
+        <img src={dcDark} alt="" data-aos="fade-up"></img>
+        <h1 data-aos="fade-up">JOIN DISCORD</h1>
+      </div>
+
+
     </div>
   );
 }
