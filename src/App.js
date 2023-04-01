1a8aa09e74b71c519d4f5229db65fd0d2091590e
Emrecan Üzüm <uzumemrecan@gmail.com>
2023-04-02 02:22:48 +0300

    discord/carousel

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import banner from './assets/banner.png';
 import fifa from './assets/fifa.png';
 import cod from './assets/cod.png';
@@ -6,6 +8,9 @@ import wow from './assets/wow.png';
 import dc from './assets/dc.png';
 import dcDark from './assets/dcb.png';
 import bg from './assets/gradient.png';
+import persona from './assets/persona.jpg';
+
+import Carousel from "react-simply-carousel";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import './App.css';
@@ -15,6 +20,8 @@ function App() {
   useEffect(() => {
     Aos.init({});
   }, [])
+
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
   return (
     <div className="App">
 
@@ -25,7 +32,7 @@ function App() {
       </div>
 
 
-      <div className='Banner' data-aos="fade-in" >
+      <div className='Banner'>
       <img src={banner} alt=""></img>,
       </div>
 
@@ -48,10 +55,81 @@ function App() {
       </div>
 
       <div className='join-discord'>
-        <img src={dcDark} alt="" data-aos="fade-up"></img>
+        <div className='hover'>
+        <a href='-'>
+          <figure>
+            <img src={dcDark} alt="" data-aos="fade-up"></img>    
+          </figure>       
+        </a>
         <h1 data-aos="fade-up">JOIN DISCORD</h1>
+        </div>
       </div>
 
+      <div className='section-carousel'>
+        <Carousel
+        activeSlideIndex={activeSlideIndex}
+        onRequestChange={setActiveSlideIndex}
+        itemsToShow={1}
+        itemsToScroll={1}
+        forwardBtnProps={{
+          //here you can also pass className, or any other button element attributes
+          style: {
+            alignSelf: 'center',
+            background: 'black',
+            border: 'none',
+            borderRadius: '50%',
+            color: 'white',
+            cursor: 'pointer',
+            fontSize: '20px',
+            height: 30,
+            lineHeight: 1,
+            textAlign: 'center',
+            width: 30,
+          },
+          children: <span>{`>`}</span>
+        }}
+        backwardBtnProps={{
+          //here you can also pass className, or any other button element attributes
+          style: {
+            alignSelf: 'center',
+            background: 'black',
+            border: 'none',
+            borderRadius: '50%',
+            color: 'white',
+            cursor: 'pointer',
+            fontSize: '20px',
+            height: 30,
+            lineHeight: 1,
+            textAlign: 'center',
+            width: 30,
+          },
+          children: <span>{`<`}</span>,
+        }}
+        responsiveProps={[
+          {
+            itemsToShow: 3,
+            itemsToScroll: 1,
+            minWidth: 100,
+          }
+        ]}
+        speed={400}
+        easing="linear">
+
+          <div className='person'>
+              <img src={persona} alt=""></img>
+          </div>
+          <div className='person'>
+              <img src={persona} alt=""></img>
+          </div>
+          <div className='person'>
+              <img src={persona} alt=""></img>
+          </div>
+          <div className='person'>
+              <img src={persona} alt=""></img>
+          </div>
+
+        </Carousel>
+      </div>
 
     </div>
   );
