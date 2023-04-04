12614eb894af3874f83b9574d82f34bde52f7152
Emrecan Üzüm <uzumemrecan@gmail.com>
2023-04-04 05:15:50 +0300

    v2.0

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { useState } from 'react';
+import React from 'react'
 
-import banner from './assets/banner.png';
+import dcw from './assets/discordW.png';
 import fifa from './assets/fifa.png';
 import cod from './assets/cod.png';
 import ow from './assets/ow.png';
 import wow from './assets/wow.png';
-import dc from './assets/dc.png';
 import dcDark from './assets/dcb.png';
-import bg from './assets/gradient.png';
+import bg from './assets/newbg.png';
+
 import persona from './assets/persona.png';
-import banner2 from './assets/banner5.png';
+import personaB from './assets/personaB.png';
+import personaP from './assets/personaP.png';
+import personaR from './assets/personaR.png';
+import personaO from './assets/personaO.png';
+
+
 import objRight from './assets/object-right.png';
 import objLeft from './assets/object-left.png';
 import fb from './assets/fb.png';
@@ -22,7 +28,9 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import './App.css';
 import './roadmap.css';
+import './assetRoadmap.css';
 import { useEffect } from 'react';
+
 
 function App() {
   useEffect(() => {
@@ -30,25 +38,84 @@ function App() {
   }, [])
 
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+
+  function teacher1() {
+    var expandImg = document.getElementById("expand");
+    expandImg.src = personaB
+    expandImg.parentElement.style.display = "block";
+
+    var header = document.getElementById("tHeader");
+    header.innerHTML="Simple";
+
+    var desc = document.getElementById("tDesc");
+    desc.innerHTML="Best CS:GO Player of all times!"
+  }
+  function teacher2() {
+    var expandImg = document.getElementById("expand");
+    expandImg.src = personaP
+    expandImg.parentElement.style.display = "block";
+
+    var header = document.getElementById("tHeader");
+    header.innerHTML="aBeZy";
+
+    var desc = document.getElementById("tDesc");
+    desc.innerHTML="Best COD Player"
+  }
+  function teacher3() {
+    var expandImg = document.getElementById("expand");
+    expandImg.src = personaR
+    expandImg.parentElement.style.display = "block";
+
+    var header = document.getElementById("tHeader");
+    header.innerHTML="JJonak";
+
+    var desc = document.getElementById("tDesc");
+    desc.innerHTML="Best Overwatch Player"
+  }
+  function teacher4() {
+    var expandImg = document.getElementById("expand");
+    expandImg.src = personaO
+    expandImg.parentElement.style.display = "block";
+
+    var header = document.getElementById("tHeader");
+    header.innerHTML="LevideWeerd";
+
+    var desc = document.getElementById("tDesc");
+    desc.innerHTML="Best FIFA 23 Player"
+  }
+
   return (
-    <div className="App">
+    <div className="App"   style={{ backgroundImage:`url(${bg})` }}>
       <div className='Row-Header'>
         <h2>gamingcourses.gg</h2>
-        <div className='hover'>
-          <a href='-'>
-            <figure>
-              <img src={dc} alt=""></img>
-            </figure>
-          </a>
+        <div className='Row-Buttons'>
+          <a href="-">MISSION</a>
+          <a href="-">TEACHERS</a>
+          <a href="-">ROADMAP</a>
         </div>
-        <h1>From Bro's to Pro's</h1>
+        <div className='Row-Discord'>
+          <div className='hover'>
+            <a href='-'>
+              <figure>
+                <img src={dcw} alt=""></img>    
+              </figure>       
+            </a>
+          </div>
+        </div>
       </div>
+
+      <hr></hr>
 
       <div className='Banner'>
-        <img src={banner} alt=""></img>,
+        <h1>LOREMIPSUM</h1>
+        <h2>From Bro's to Pro's</h2>
+        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </h4>
       </div>
 
-      <div className="Landing-Bottom"  style={{ backgroundImage:`url(${bg})` }}>
+      <div className="Landing-Bottom">
         <div className="Row"  data-aos="fade-up">
           <div className="index-6-1">
             <img src={fifa} alt=""></img>
@@ -65,6 +132,23 @@ function App() {
         </div>
       </div>
 
+      <div className='mission'>
+        <div className='mission-header' data-aos="fade-up">
+          <h2>MISSION</h2>
+        </div>
+        <div className='mission-context' data-aos="fade-up">
+          <img src={objLeft} alt=""></img>
+          <div className='mission-box' data-aos="fade-up">
+              <h6>
+                Our mission is connect esports gaming PRO's with BRO's, 
+                provide valuable video lessons material to our subscribers 
+                while keeping our competitive esports gaming community happy and updated.
+                </h6>
+          </div>
+          <img src={objRight} alt=""></img>
+        </div> 
+      </div>
+
       <div className='join-discord'>
         <div className='hover'>
         <a href='-'>
@@ -76,10 +160,14 @@ function App() {
         </div>
       </div>
 
-      <div className='teachers'  style={{ backgroundImage:`url(${banner2})`}}>
-        <h1  data-aos="fade-up">TEACHERS</h1>
-        <h6  data-aos="fade-up">This could be you</h6>
+      <div className='teachers'>
+          <h1 id="tHeader" data-aos="fade-up">TEACHERS</h1>
+          <h6 id="tDesc" data-aos="fade-up">This could be you</h6>
       </div>  
+      <div className='teacher-face'>
+        <img src={persona} alt="" id="expand"></img>
+      </div>
+      
       <div className='section-carousel' data-aos="fade-up">
         <a href='-'>
           <div className='t-button'  data-aos="fade-up">
@@ -135,53 +223,78 @@ function App() {
         ]}
         speed={400}
         easing="linear">
-          <div className='person' >
-              <img src={persona} alt=""></img>
-          </div>
-          <div className='person'>
-              <img src={persona} alt=""></img>
-          </div>
-          <div className='person'>
-              <img src={persona} alt=""></img>
-          </div>
-          <div className='person'>
-              <img src={persona} alt=""></img>
-          </div>
-        </Carousel>
-      </div>
 
-      <div className='mission'>
-        <div className='mission-header' data-aos="fade-up">
-          <h2>MISSION</h2>
-        </div>
-        <div className='mission-context' data-aos="fade-up">
-          <img src={objLeft} alt=""></img>
-          <div className='mission-box' data-aos="fade-up">
-              <h6>
-                Our mission is connect esports gaming PRO's with BRO's, 
-                provide valuable video lessons material to our subscribers 
-                while keeping our competitive esports gaming community happy and updated.
-                </h6>
+          <div className='person' >
+            <img id="persona" src={personaB} alt="" onClick={teacher1}></img>
           </div>
-          <img src={objRight} alt=""></img>
-        </div> 
+          <div className='person'>
+            <img id="persona" src={personaP} alt="" onClick={teacher2}></img>
+          </div>
+          <div className='person'>
+            <img id="persona" src={personaR} alt="" onClick={teacher3}></img>
+          </div>
+          <div className='person'>
+            <img id="persona" src={personaO} alt="" onClick={teacher4}></img>
+          </div>
+
+        </Carousel>  
       </div>
+      
       
 
       <section id="roadmap" data-aos="fade-up">
         <h3 class="roadmap-title" data-aos="fade-up">
             ROADMAP
         </h3>
-        <main data-aos="fade-up">
-          <p>Stage 1</p>
-          <p>Stage 2</p>
-          <p>Stage 3</p>
-          <p>Stage 4</p>
-          <p>Stage 5</p>
-          
-        </main>
       </section>
       
+      <section id="cd-timeline" class="cd-container">
+		  <div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-picture">
+			</div>
+
+			<div class="cd-timeline-content">
+        <h2>Title of section 2</h2>
+			  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde?</p> 
+        <span class="cd-date">Jan 13</span>
+      </div>
+		  </div>
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+			</div>
+
+			<div class="cd-timeline-content">
+				<h2>Title of section 2</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde?</p>
+				<span class="cd-date">Jan 18</span>
+			</div>
+		</div>
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-picture">
+			</div> 
+
+			<div class="cd-timeline-content">
+				<h2>Title of section 3</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, obcaecati, quisquam id molestias eaque asperiores voluptatibus cupiditate error assumenda delectus odit similique earum voluptatem doloremque dolorem ipsam quae rerum quis. Odit, itaque, deserunt corporis vero ipsum nisi eius odio natus ullam provident pariatur temporibus quia eos repellat consequuntur perferendis enim amet quae quasi repudiandae sed quod veniam dolore possimus rem voluptatum eveniet eligendi quis fugiat aliquam sunt similique aut adipisci.</p>
+				<span class="cd-date">Jan 24</span>
+			</div> 
+		</div>
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+			</div>
+
+			<div class="cd-timeline-content">
+				<h2>Title of section 4</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span class="cd-date">Feb 14</span>
+			</div>
+		</div>
+
+		
+	</section> 
       <div className='footer'  data-aos="fade-up">
         <div className='footer-text'>
           <h6>Follow us on social media.</h6>
